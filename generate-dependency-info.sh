5662cefdaa2d54ea402163fd39e1e1012c0905a4
Rodrigo Witzel <rwitzel75@googlemail.com>
2015-03-24 17:21:39 +0100

    improved: list of runtime dependencies is sorted now

--- generate-dependency-info.sh
+++ generate-dependency-info.sh
@@ -2,6 +2,6 @@
 rm -f generated-runtime-deps-list.txt
 rm -f generated-runtime-deps-tree.txt
 
-mvn dependency:list -DincludeScope=runtime -DoutputFile=generated-runtime-deps-list.txt
+mvn dependency:list -DincludeScope=runtime -DoutputFile=generated-runtime-deps-list.txt -Dsort=true
 mvn dependency:tree -Dscope=runtime -DoutputFile=generated-runtime-deps-tree.txt
  
